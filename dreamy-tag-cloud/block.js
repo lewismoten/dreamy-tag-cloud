37aa076aaaabeef1b6447ed2f84f148f22f3fae6
Lewis Moten <lewismoten@gmail.com>
2026-01-17 20:08:09 -0500

    put preview in block props, fix auto-exclude

--- dreamy-tag-cloud/block.js
+++ dreamy-tag-cloud/block.js
@@ -1,75 +1,72 @@
 (({
-    serverSideRender,
-    blocks: {
-      registerBlockType
-    },
-    element: {
-      createElement: el
-    },
-    blockEditor: {
-      InspectorControls, 
-      useBlockProps
-    },
-    components: {
-      PanelBody,
-      TextControl,
-      TextareaControl,
-      ToggleControl
-    }
-  }) => {
+  serverSideRender,
+  blocks: { registerBlockType },
+  element: { createElement: el },
+  blockEditor: { InspectorControls, useBlockProps },
+  components: { PanelBody, TextControl, TextareaControl, ToggleControl }
+}) => {
 
   const pluginName = 'dreamy-tag-cloud';
   const typeName = `lewismoten/${pluginName}`;
 
-  const toNumbers = value => (value || "")
-      .split(",")
-      .map(s => s.trim())
-      .filter(s => !isNaN(s))
-      .filter(Boolean)
-      .map(s => parseInt(s, 10));
+  const toNumbers = (value) => (value || "")
+    .split(",")
+    .map(s => s.trim())
+    .filter(Boolean)
+    .filter(s => !isNaN(s))
+    .map(s => parseInt(s, 10));
 
   registerBlockType(typeName, {
-    edit:  (props) => {
+    edit: (props) => {
       const attrs = props.attributes;
-      const blockProps = useBlockProps({ className: `${pluginName}-editor` });
-      return [
-        el('div', blockProps,
-          el(InspectorControls, { key: "inspector" },
-            el(PanelBody, { title: "Dreamy Tags Settings", initialOpen: true },
 
-              el(TextControl, {
-                label: "Title",
-                value: attrs.title || "",
-                onChange: (v) => props.setAttributes({ title: v }),
-              }),
+      const blockProps = useBlockProps({
+        className: `${pluginName}-editor`
+      });
 
-              el(TextareaControl, {
-                label: "Filter Categories",
-                help: "Example: 3, 9",
-                value: attrs.cat_raw || "",
-                onChange: (v) => props.setAttributes({ cat_raw: v }),
-                onBlur: () =>
-                  props.setAttributes({ cat: toNumbers(attrs.cat_raw) })
-              }),
+      return el(
+        'div',
+        blockProps,
+        el(
+          InspectorControls,
+          { key: "inspector" },
+          el(
+            PanelBody,
+            { title: "Dreamy Tags Settings", initialOpen: true },
 
-              el(TextareaControl, {
-                label: "Filter Tags",
-                value: attrs.tags_raw || '',
-                onChange: (v) => props.setAttributes({ tags_raw: v }),
-                onBlur:  () => props.setAttributes({ tags : toNumbers(attrs.tags_raw)})
-              }),
-              el(ToggleControl, {
-                label: "Auto-exclude filtered",
-                checked: !!attrs.exclude,
-                onChange: (v) => props.setAttributes({ exclude: v }),
-              }),
-              el(TextareaControl, {
-                label: "Exclude Tags",
-                value: attrs.exclude_raw || '',
-                onChange: (v) => props.setAttributes({ exclude_raw: v }),
-                onBlur: () => props.setAttributes({ exclude: toNumbers(attrs.exclude_raw) }),
-              })
-            )
+            el(TextControl, {
+              label: "Title",
+              value: attrs.title || "",
+              onChange: (v) => props.setAttributes({ title: v }),
+            }),
+
+            el(TextareaControl, {
+              label: "Filter Categories",
+              help: "Example: 3, 9",
+              value: attrs.cat_raw || "",
+              onChange: (v) => props.setAttributes({ cat_raw: v }),
+              onBlur: () => props.setAttributes({ cat: toNumbers(attrs.cat_raw) }),
+            }),
+
+            el(TextareaControl, {
+              label: "Filter Tags",
+              value: attrs.tags_raw || "",
+              onChange: (v) => props.setAttributes({ tags_raw: v }),
+              onBlur: () => props.setAttributes({ tags: toNumbers(attrs.tags_raw) }),
+            }),
+
+            el(ToggleControl, {
+              label: "Auto-exclude filtered tags",
+              checked: !!attrs.auto_exclude,
+              onChange: (v) => props.setAttributes({ auto_exclude: v }),
+            }),
+
+            el(TextareaControl, {
+              label: "Exclude Tags",
+              value: attrs.exclude_raw || "",
+              onChange: (v) => props.setAttributes({ exclude_raw: v }),
+              onBlur: () => props.setAttributes({ exclude: toNumbers(attrs.exclude_raw) }),
+            })
           )
         ),
 
@@ -77,10 +74,11 @@
           key: "preview",
           block: typeName,
           attributes: attrs,
-        }),
-      ];
+        })
+      );
     },
 
     save: () => null
   });
+
 })(window.wp);
